583b43e3ffe7643ec1bb2be1bfe826cb3e5d01c5
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2021-02-17 16:02:15 -0300

    setup

---
+++ seriesGuide/src/main/java/com/stags/seriesGuide/SeriesGuideApplication.java
@@ -0,0 +1,13 @@
+package com.stags.seriesGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesGuideApplication.class, args);
+	}
+
+}
---
+++ seriesGuide/src/test/java/com/stags/seriesGuide/SeriesGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stags.seriesGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
